09a041ea2e1be55051de240d0999d42fd1a6551c
Synaxis <01xtor@gmail.com>
2018-02-11 20:49:27 -0200

    variables

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
+var circleX = 150;
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+createCanvas(600, 400);
 }
 
-function draw() {
-
+function draw() {  
+background(250, 250, 100);
+fill(250, 200, 200, 50);
+ellipse(circleX, 200, 80, 80);
 }
